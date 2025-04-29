5847b04d74c13f3cbc0d599b176621677d237c97
Rafael Lacerda <Rafaellacerda2004@gmail.com>
2025-04-29 17:47:59 -0300

    const PORT = process.env.PORT || 3000;

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const DB_FILE = path.join(__dirname, 'db.json');
 
 // Habilita leitura JSON e arquivos estáticos da pasta public
